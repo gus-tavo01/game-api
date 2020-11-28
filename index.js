57c6c3474f25d580aa5d843b096ca5da1d3d9187
Gustavo loera <gustavoa.loera01@gmail.com>
2020-11-27 23:52:56 -0800

    add auth controller

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ import dotenv from 'dotenv';
 import express from 'express';
 
 // routes
-import { characters } from './controllers/characters.js';
+import { authController } from './controllers/auth.controller.js';
+import { charactersController } from './controllers/characters.controller.js';
 
 dotenv.config();
 
+const apiPrefix = '/api/v1';
 const port = process.env.PORT || 3000;
 const app = express();
 
 // arrange controllers to routes
-app.use('/characters', characters);
+app.use(`${apiPrefix}/auth`, authController);
+app.use(`${apiPrefix}/characters`, charactersController);
 
 app.listen(port);
