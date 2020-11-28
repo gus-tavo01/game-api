fc627c825a02e6af2b53427fb893c5281114d830
Gustavo loera <gustavoa.loera01@gmail.com>
2020-11-27 23:59:04 -0800

    add api response methods

---
+++ common/ApiResponse.js
@@ -0,0 +1,55 @@
+export class ApiResponse {
+  statusCode = 200;
+  message = 'Success';
+  errorMessage = null;
+  fields = []; // map this from service validations
+  result = null;
+
+  ok = (payload) => {
+    this.statusCode = 200;
+    this.message = 'Success';
+    this.result = payload;
+  };
+
+  created = (payload) => {
+    this.statusCode = 201;
+    this.message = 'Created';
+    this.result = payload;
+  };
+
+  badRequest = (errorMessage) => {
+    this.statusCode = 400;
+    this.message = 'Bad_Request';
+    this.errorMessage = errorMessage;
+  };
+
+  forbidden = (errorMessage) => {
+    this.statusCode = 401;
+    this.message = 'Forbidden';
+    this.errorMessage = errorMessage;
+  };
+
+  notFound = (errorMessage) => {
+    this.statusCode = 404;
+    this.message = 'Not_Found';
+    this.errorMessage = errorMessage;
+  };
+
+  conflict = (errorMessage) => {
+    this.statusCode = 409;
+    this.message = 'Conflict';
+    this.errorMessage = errorMessage;
+  };
+
+  unprocessableEntity = (errorMessage) => {
+    this.statusCode = 422;
+    this.message = 'Unprocessable_Entity';
+    this.errorMessage = errorMessage;
+  };
+
+  serverError = (errorMessage) => {
+    this.statusCode = 500;
+    this.message = 'Server_Error';
+    this.errorMessage = errorMessage;
+  };
+}
