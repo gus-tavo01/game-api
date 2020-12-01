c1472524834c46d69bbd3dbea32829777eb15735
Gustavo loera <gustavoa.loera01@gmail.com>
2020-11-30 22:50:53 -0800

    add passport configuration function

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,20 @@
-const passport = require('passport');
-// user repo/service?
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+const AccountsRepository = require('../repositories/accounts.repository');
 
-// TODO
-// configure passport auth strategy
-// configure auth conditions
+function configureStrategy(passport) {
+  const accountsRepository = new AccountsRepository();
+  const options = {
+    ignoreExpiration: false,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  };
+  const jwtStrategy = new JwtStrategy(options, (jwt_payload, done) => {
+    accountsRepository.findOne(jwt_payload.sub).then((account) => {
+      done(null, account);
+    });
+  });
+  passport.use(jwtStrategy);
+}
+
+module.exports = configureStrategy;
