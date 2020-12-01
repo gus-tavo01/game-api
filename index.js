d35a0444bec02a075920b5b28f55f2c4a08cd08b
Gustavo loera <gustavoa.loera01@gmail.com>
2020-11-30 22:43:19 -0800

    add passport middleware

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const passport = require('passport');
 const routes = require('./routes/index');
+// configurations
+const configureStrategy = require('./config/passport');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 // setup db, passport
+configureStrategy(passport);
+// configure DB
 
 // add middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes);
+app.use(passport.initialize());
 
 app.listen(port);
