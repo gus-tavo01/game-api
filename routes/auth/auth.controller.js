591e944f2b6a89948f341915a1977cc2a6d521a4
Gustavo loera <gustavoa.loera01@gmail.com>
2020-11-30 23:06:41 -0800

    add basic flow to login method

--- routes/auth/auth.controller.js
+++ routes/auth/auth.controller.js
@@ -1,7 +1,49 @@
-class AuthController {
-  login = (username, password) => {};
+const jsonwebtoken = require('jsonwebtoken');
+const ApiResponse = require('../../common/ApiResponse');
+// use service instead repos
+const AccountsRepository = require('../../repositories/accounts.repository');
 
-  createLoginCredentials = () => {};
+class AuthController {
+  constructor() {
+    this.accountsRepository = new AccountsRepository();
+  }
+
+  login = async (req, res) => {
+    const apiResponse = new ApiResponse();
+    const { username, password } = req.body;
+    const accountsResponse = await this.accountsRepository.find({ username });
+    const account = accountsResponse[0];
+
+    if (!account) {
+      apiResponse.unauthorized('Invalid credentials');
+      return res.status(apiResponse.statusCode).json(apiResponse);
+    }
+
+    // TODO
+    // hash body.password
+    // compare hashed result with DB stored password
+    // if false, return 401 response
+
+    const secret = process.env.JWT_SECRET;
+    const expiresIn = '2h';
+    const payload = {
+      iat: Date.now(),
+      sub: account.id,
+    };
+    const token = jsonwebtoken.sign(payload, secret, { expiresIn });
+    const result = { token, expiresIn };
+    apiResponse.ok(result);
+    return res.status(200).json(apiResponse);
+  };
+
+  createLogin = (req, res) => {};
+
+  // TODO:
+  // delete this  dummy route
+  getProfile = (req, res) => {
+    const { user } = req;
+    res.status(200).json(user);
+  };
 }
 
 module.exports = AuthController;
