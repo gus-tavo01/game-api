5ab0580ebb284032c2ceb005f05b4e41aa9fc37b
Gustavo loera <gustavoa.loera01@gmail.com>
2020-12-01 21:26:25 -0800

    resolve dependencies, service, controller, repositories

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,23 @@ const charactersRoute = require('./characters/characters.route.js');
 // controllers
 const CharactersController = require('./characters/characters.controller.js');
 const AuthController = require('./auth/auth.controller.js');
-
 // services
 const CharactersService = require('../services/characters.service.js');
+const AccountsService = require('../services/accounts.service.js');
+// repositories
+const AccountsRepository = require('../repositories/accounts.repository');
 
 const router = Router();
 const apiPrefix = '/api/v1';
 
 // resolve dependencies
+const accountsRepository = new AccountsRepository();
+// services
 const charactersService = new CharactersService();
+const accountsService = new AccountsService(accountsRepository);
+// controllers
 const charactersController = new CharactersController(charactersService);
-const authController = new AuthController();
+const authController = new AuthController(accountsService);
 
 // endpoints
 router.use(`${apiPrefix}/auth`, authRoute(authController));
