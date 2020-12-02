4e881aa9c4ee5365815cd1bf0f060301e38ec301
Gustavo loera <gustavoa.loera01@gmail.com>
2020-12-01 21:39:33 -0800

    add mockie repo methods

--- repositories/accounts.repository.js
+++ repositories/accounts.repository.js
@@ -1,15 +1,28 @@
 class AccountsRepository {
-  accounts = [{ id: 2, username: 'yayis', password: 'password!' }];
+  accounts = [
+    {
+      id: 2,
+      username: 'ticky',
+      passwordHash:
+        '$2b$10$A0ka6mNlRIw.Qa4mN3x3OOGU1JfEv03Nwl.GhRyesL2on84L8HyKO',
+    },
+  ];
 
   find = async (filters) => {
     const { username } = filters;
-    const result = this.accounts.find((l) => l.username === username);
+    const result = this.accounts.find((acc) => acc.username === username);
     return [result];
   };
 
   findOne = async (id) => {
-    return this.accounts.find((a) => a.id === id);
+    return this.accounts.find((acc) => acc.id === id);
   };
+
+  add = async (account) => {
+    this.accounts.push(account);
+    return this.accounts.length;
+  };
+
   // remove
   // update
 }
