d3ffd1b5ee5a6476d19cb9fd51fc5e4b78bfb002
Gustavo loera <gustavoa.loera01@gmail.com>
2020-12-01 21:40:23 -0800

    remove protected dummy route

--- routes/auth/auth.route.js
+++ routes/auth/auth.route.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
-const bearerAuth = require('../../helpers/addBearerAuth');
 
 const authRouter = (controller) => {
   const router = Router();
   router.post('/login', controller.login);
   router.post('/register', controller.createLogin);
-  router.get('/profile', bearerAuth(), controller.getProfile);
   return router;
 };
 
