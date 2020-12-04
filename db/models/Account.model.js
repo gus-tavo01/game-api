7574481fb0d8e4ed79ce1587b89330aa59d8fa0d
Gustavo loera <gustavoa.loera01@gmail.com>
2020-12-03 17:56:44 -0800

    add extra properties on account model schema

--- db/models/Account.model.js
+++ db/models/Account.model.js
@@ -13,6 +13,21 @@ const AccountSchema = new Schema({
     type: String,
     required: false,
   },
+  createDate: {
+    type: Date,
+    required: false,
+    default: new Date(),
+  },
+  lastLoginDate: {
+    type: Date,
+    required: true,
+    default: null,
+  },
+  isActive: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
 });
 
 const Account = model('Account', AccountSchema);
