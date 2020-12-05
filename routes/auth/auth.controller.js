bc0fbbb62b85a762f249280a84e17a8ac21a4daa
Gustavo loera <gustavoa.loera01@gmail.com>
2020-12-05 14:05:25 -0800

    remove create login from auth controller

--- routes/auth/auth.controller.js
+++ routes/auth/auth.controller.js
@@ -53,57 +53,6 @@ class AuthController {
       return res.status(apiResponse.statusCode).json(apiResponse);
     }
   };
-
-  createLogin = async (req, res) => {
-    const { username, password } = req.body;
-    const apiResponse = new ApiResponse();
-    try {
-      const getAccountResponse = await this.accountsService.getByUsername(
-        username
-      );
-
-      if (!getAccountResponse.fields) {
-        apiResponse.badRequest(
-          'Please check your credentials for errors',
-          getAccountResponse.fields
-        );
-        return res.status(apiResponse.statusCode).json(apiResponse);
-      }
-
-      if (!!getAccountResponse.result) {
-        apiResponse.conflict(`Username: '${username}' already exists`);
-        return res.status(apiResponse.statusCode).json(apiResponse);
-      }
-
-      const salt = await bcrypt.genSalt();
-      const passwordHash = await bcrypt.hash(password, salt);
-      const accountRequest = { username, passwordHash };
-      const postAccountResponse = await this.accountsService.post(
-        accountRequest
-      );
-
-      if (postAccountResponse.fields.length) {
-        apiResponse.badRequest(
-          'Please check your data for errors',
-          postAccountResponse.fields
-        );
-        return res.status(apiResponse.statusCode).json(apiResponse);
-      }
-
-      if (!postAccountResponse.result && !postAccountResponse.fields.length) {
-        apiResponse.unprocessableEntity(
-          'The account cannot be added, please try again later'
-        );
-        return res.status(apiResponse.statusCode).json(apiResponse);
-      }
-
-      apiResponse.created(postAccountResponse.result);
-      return res.status(apiResponse.statusCode).json(apiResponse);
-    } catch (error) {
-      apiResponse.internalServerError(error.message);
-      return res.status(apiResponse.statusCode).json(apiResponse);
-    }
-  };
 }
 
 module.exports = AuthController;
