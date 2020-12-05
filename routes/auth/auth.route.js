efccaf499edfe207f5fc675b67191df0624e12f1
Gustavo loera <gustavoa.loera01@gmail.com>
2020-12-05 13:33:38 -0800

    remove register route

--- routes/auth/auth.route.js
+++ routes/auth/auth.route.js
@@ -3,7 +3,6 @@ const { Router } = require('express');
 const authRouter = (controller) => {
   const router = Router();
   router.post('/login', controller.login);
-  router.post('/register', controller.createLogin);
   return router;
 };
 
